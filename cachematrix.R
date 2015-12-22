33890e151885f2cc18ac3a460721d611e67d10d2
Nathan Malone <nathanm@ptsi.net>
2015-12-22 06:05:16 -0600

    <submittal of assignment 2>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(x=matrix()) {         #This function returns an 
+  nvr=NULL                                        #     inverse of a matrix
+      set=function(y) {
+            x <<- y                               #assigns a value to an object 
+            nvr<<-NULL                            #     that is in a different
+      }                                           #     environment
+      get=function() x
+      setnvr=function(inverse) nvr<<-inverse      #list of functions are used as
+      getnvr=function() nvr                       #     the input to cacheSolve()
+      list(set=set, get=get, setnvr=setnvr, getnvr=getnvr)
+      }
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+
+cacheSolve <- function(x, ...) {                   #This function returns the 
+      nvr=x$getnvr()                               #    inverse of the matrix
+      if(!is.null(nvr)){                           #if inverse has been calcualated,
+      message("getting cached data")               #    this calculagtion is skipped
+      return(nvr)
+      }
+      mat.data<-x$get()                            #otherwise, the inverse is 
+                                                   #   calculated
+      nvr<-solve(mat.data, ...)
+    
+      x$setnvr(nvr)                                #sets the value of the inverse
+      return(nvr)                                  #   in the chache
 
 }
 
 
-## Write a short comment describing this function
+##sample run
+##x=rbind(c(5, -2/4), c(-2/4, 5))
+##m=makeCacheMatrix(x)
+##m$get()
+##     [,1] [,2]
+##[1,]  5.0 -0.5
+##[2,] -0.5  5.0
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##cachesolve(m)
+##           [,1]       [,2]
+##[1,] 0.20202020 0.02020202
+##[2,] 0.02020202 0.20202020
